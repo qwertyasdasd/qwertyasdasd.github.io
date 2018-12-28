d27791f883d982f5badcd7a627233efc882f711d
xamel1on <46139957+xamel1on@users.noreply.github.com>
2018-12-28 03:50:45 +0300

    w43124323

---
+++ wp-login.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="ru-RU">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="ru-RU">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Войти &lsaquo; testwordpress &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' id='dashicons-css'  href='http://localhost/http:/blog2019.tk/testwordpress/wp-includes/css/dashicons.min.css?ver=5.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='http://localhost/http:/blog2019.tk/testwordpress/wp-includes/css/buttons.min.css?ver=5.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='http://localhost/http:/blog2019.tk/testwordpress/wp-admin/css/forms.min.css?ver=5.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='http://localhost/http:/blog2019.tk/testwordpress/wp-admin/css/l10n.min.css?ver=5.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='http://localhost/http:/blog2019.tk/testwordpress/wp-admin/css/login.min.css?ver=5.0.2' type='text/css' media='all' />
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-ru-ru">
+		<div id="login">
+		<h1><a href="https://ru.wordpress.org/" title="Сайт работает на WordPress" tabindex="-1">Сайт работает на WordPress</a></h1>
+	
+<form name="loginform" id="loginform" action="http://localhost/http:/blog2019.tk/testwordpress/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Имя пользователя или e-mail<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Пароль<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Запомнить меня</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Войти" />
+		<input type="hidden" name="redirect_to" value="/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="http://localhost/http:/blog2019.tk/testwordpress/wp-login.php?action=lostpassword">Забыли пароль?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="http://localhost/http:/blog2019.tk/testwordpress/">&larr; Назад к сайту &laquo;testwordpress&raquo;</a></p>
+		
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
